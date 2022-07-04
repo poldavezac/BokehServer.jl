bd8d5caf2f65a299be0b0ca064e4610491fd64c5
Pol d'Avezac <pdavezac@yahoo.fr>
2022-07-04 18:37:17 +0200

    notebook example

--- examples/notebook.jl
+++ examples/notebook.jl
@@ -15,7 +15,9 @@ end
 fig = Plotting.line(; x = 1:10, y = 1:10)
 
 # ╔═╡ b2ea7ada-53bb-44c9-9123-9d5ef1ab59b2
-fig.renderers[1].data_source.data
+let data = fig.renderers[1].data_source.data
+	push!(data, Dict("x" => [length(data["x"])], "y" => [rand(1:10)]))
+end
 
 # ╔═╡ Cell order:
 # ╠═d4981f9e-fab5-4f64-957c-fb8c67fa80b6
