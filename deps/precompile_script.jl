9bd279f1173face610d6f7e1367026cd19b32384
Pol d'Avezac <pdavezac@yahoo.fr>
2022-07-26 11:37:51 +0200

    refactoring plotting/tools.jl

--- deps/precompile_script.jl
+++ deps/precompile_script.jl
@@ -5,10 +5,12 @@ HTTP = BokehServer.Server.HTTP
     a::Int = 1
 end
 dom = ServerTestDOM(; a = 100)
+@info "notebook script: server"
 show(IOBuffer(), MIME("text/html"), dom)
 
 sleep(0.01)
 
+@info "notebook script: client"
 BokehServer.Client.open(BokehServer.Embeddings.Notebooks.lastws()) do hdl::BokehServer.Client.MessageHandler
     doc = hdl.doc
     task_local_storage(BokehServer.Events.TASK_EVENTS, BokehServer.Events.EVENTS[]) do
@@ -24,6 +26,7 @@ sleep(0.01)
 BokehServer.Embeddings.Notebooks.stopserver()
 sleep(0.01)
 
+@info "server script: server"
 server = HTTP.Sockets.listen(HTTP.Sockets.InetAddr(HTTP.Sockets.ip"127.0.0.1", 5006))
 task = @async BokehServer.Server.serve(
     5006,
@@ -38,6 +41,7 @@ task = @async BokehServer.Server.serve(
 )
 sleep(0.01)
 
+@info "server script: client"
 BokehServer.Client.open("ws://localhost:5006/app/ws") do _, doc
     BokehServer.varea!(
         doc.roots[1];
@@ -48,6 +52,7 @@ BokehServer.Client.open("ws://localhost:5006/app/ws") do _, doc
 end
 close(server)
 
+@info "html script"
 BokehServer.html(; browser = false, path = tempdir()* "/toto.html") do
     labels = [j == 1 ? "x" : "y" for j = 1:2 for _ = 1:(j*10)]
     vals   = [(randn(Float64, 10) .+ 10.)..., (randn(Float64, 20) .- 10.)...]
