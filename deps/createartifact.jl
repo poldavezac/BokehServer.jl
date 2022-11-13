23cf95c6a7bc495ceb6e7307657eb7d0de26c316
Pol d'Avezac <pol.davezac@gmail.com>
2022-11-13 22:51:09 +0100

    adding access to conda's bokeh3

--- deps/createartifact.jl
+++ deps/createartifact.jl
@@ -7,7 +7,7 @@ using Tar
 artifact_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
 for (name, url_base) ∈ (
-    "javascript" => "https://anaconda.org/bokeh/bokeh/2.4.3/download/noarch/bokeh-2.4.3-py_0.tar.bz2",
+    "javascript" => "https://anaconda.org/bokeh/bokeh/3.0.1/download/noarch/bokeh-3.0.1-py_0.tar.bz2",
     #"nodejs" => "https://nodejs.org/dist/v16.17.1/node-v16.17.1-linux-x64.tar.xz"
 )
     js_hash       = artifact_hash(name, artifact_toml)
